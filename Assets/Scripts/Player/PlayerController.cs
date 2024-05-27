e30b34b09fc9cab90117c7b35108409717f3f204
jhj603 <82034869+jhj603@users.noreply.github.com>
2024-05-28 00:55:12 +0900

    [feat]assets

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -40,10 +40,10 @@ public class PlayerController : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
         
-    }
+    //}
 
     // Update is called once per frame
     void FixedUpdate()
